ad7bb215a6f1a0012f154b33f6126974cd5da2d6
Ishan-001 <axil.ishan3@gmail.com>
2021-05-01 02:27:00 +0530

    Added models

---
+++ src/database/models/Team.ts
@@ -0,0 +1,40 @@
+import { Schema, model, Document } from 'mongoose';
+import User from './User';
+
+export const DOCUMENT_NAME = 'Team';
+export const COLLECTION_NAME = 'teams';
+
+export default interface Team extends Document {
+  teamCode: string;
+  users: [];
+  invited: [];
+  name: string;
+  problemStatement: string;
+  tries: number;
+}
+
+const schema = new Schema(
+  {
+    teamCode: {
+      type: Schema.Types.String,
+      required: true,
+      primary: true
+    },
+    name: {
+      type: Schema.Types.String,
+      default: true,
+    },
+    users: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+    invited: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    }],
+    problemStatement: Schema.Types.String,
+    tries: Schema.Types.Number
+  }
+);
+
+export const teamModel = model<Team>(DOCUMENT_NAME, schema, COLLECTION_NAME);
